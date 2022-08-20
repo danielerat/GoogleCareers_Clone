10cd8538e469136a33b64959e4f787f873936156
danielerat <davidodo2015@gmail.com>
2022-08-20 04:17:14 +0200

    Another example of written codes

--- src/playground.js
+++ src/playground.js
@@ -1,3 +1,5 @@
 export const evenOrOdd = (number) => {
   return number % 2 === 0 ? "Even" : "Odd";
 };
+
+export const multiply = (num1, num2) => num1 * num2;
--- tests/unit/playground.test.js
+++ tests/unit/playground.test.js
@@ -1,4 +1,4 @@
-import { evenOrOdd } from "@/playground";
+import { evenOrOdd, multiply } from "@/playground";
 describe("Basic Math", () => {
   it("Shouls add two Numbers", () => {
     expect(1 + 1).toBe(2);
@@ -15,7 +15,13 @@ describe("Basic Math", () => {
 
   describe("When the number is odd", () => {
     it("Indicate the numer is odd", () => {
-      expect(evenOrOdd(4)).toBe("Odd");
+      expect(evenOrOdd(3)).toBe("Odd");
+    });
+  });
+
+  describe("Multiply", () => {
+    it("Multiply two number together", () => {
+      expect(multiply(2, 4)).toBe(8);
     });
   });
 });
