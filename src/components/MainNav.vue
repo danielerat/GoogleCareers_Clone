d61c2a90aea127e5445c7b9912de0b31fd2edb87
danielerat <davidodo2015@gmail.com>
2022-08-24 00:27:52 +0200

    Writting and looping over out menu

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,9 +4,20 @@
       <div
         class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
       >
-        <a href="/" class="flex items-center px-4 h-full text-xl">
+        <a :href="url" class="flex items-center px-4 h-full text-xl">
           {{ company }}
         </a>
+        <nav class="h-full border ml-12">
+          <ul class="flex h-full p-0 m-0">
+            <li
+              v-for="item in menuItems"
+              :key="item"
+              class="flex border h-full border-orange-600 ml-9"
+            >
+              <a href="" class="flex items-center border">{{ item }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -19,6 +30,15 @@ export default {
     return {
       company: "Careers Junkie",
       author: { firstName: "Ilunga", lastName: "Gisa Daniel" },
+      menuItems: [
+        "Team",
+        "Locations",
+        "Life at Junkies",
+        "How we hire",
+        "Student",
+        "Jos",
+      ],
+      url: "http://www.careersjobs.com",
     };
   },
 };
