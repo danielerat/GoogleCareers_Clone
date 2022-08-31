0024db4b58322d42b743d9b10ca4d0faad3d8c72
danielerat <davidodo2015@gmail.com>
2022-08-31 11:43:58 +0200

    Adding a more sophisticated property in an object form

---
+++ tests/unit/components/ActionButton.test.js
@@ -0,0 +1,24 @@
+import { mount } from "@vue/test-utils";
+
+import ActionButton from "@/components/ActionButton";
+
+describe("Action Button", () => {
+  it("Renders Text", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        text: "Clickable",
+      },
+    });
+    expect(wrapper.text()).toMatch("Clickable");
+  });
+  it("Applies one of several styles to button", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        text: "Clickable",
+        type: "primary",
+      },
+    });
+    const button = wrapper.find("button");
+    expect(button.classes("primary")).toBe(true);
+  });
+});
