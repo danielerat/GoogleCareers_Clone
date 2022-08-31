4d8e585690b6f9a8971fb0d73618a74ad04f1770
danielerat <davidodo2015@gmail.com>
2022-08-31 12:08:31 +0200

    Creating our sub Navigation

---
+++ src/components/Subnav.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-full h-16 border-b border-solid border-brand-gray-1">
+    <div class="flex items-center h-full px-8">This is a subNav</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Subnav",
+};
+</script>
