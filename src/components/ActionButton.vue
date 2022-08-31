60c537fa86155bd701d5ad1abc494f698a7a3745
danielerat <davidodo2015@gmail.com>
2022-08-31 10:32:11 +0200

    Action button codes

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button :class="buttonClass">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ActionButton",
+  props: ["text", "type"],
+
+  computed: {
+    buttonClass() {
+      return {
+        [this.type]: true,
+      };
+    },
+  },
+};
+</script>
+<style scoped>
+button {
+  @apply px-5 py-3 rounded font-medium;
+}
+.primary {
+  @apply text-white bg-brand-blue-1 hover:shadow-blue;
+}
+.secondary {
+  @apply text-brand-blue-1 bg-transparent hover:bg-brand-blue-2 hover:text-white;
+}
+</style>
