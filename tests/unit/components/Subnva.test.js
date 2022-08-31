21ab9fc6f4f1828fa2c55310b4f7c3dfe49078a2
danielerat <davidodo2015@gmail.com>
2022-08-31 13:24:15 +0200

    Writting tests for our sub nav

---
+++ tests/unit/components/Subnva.test.js
@@ -0,0 +1,41 @@
+import { mount } from "@vue/test-utils";
+import Subnav from "@/components/Subnav";
+
+describe("Sub Nav Test", () => {
+  describe("When User is on Job page", () => {
+    it("Displays Job Count", () => {
+      const wrapper = mount(Subnav, {
+        global: {
+          stubs: {
+            FontAwesomeIcon: true,
+          },
+        },
+        data() {
+          return {
+            onJobResultsPage: true,
+          };
+        },
+      });
+      const jobCount = wrapper.find("[data-test='job-count']");
+      expect(jobCount.exists()).toBe(true);
+    });
+  });
+  describe("When User is on Not on JobSerach page", () => {
+    it("Do not display Job Count", () => {
+      const wrapper = mount(Subnav, {
+        global: {
+          stubs: {
+            FontAwesomeIcon: true,
+          },
+        },
+        data() {
+          return {
+            onJobResultsPage: false,
+          };
+        },
+      });
+      const jobCount = wrapper.find("[data-test='job-count']");
+      expect(jobCount.exists()).toBe(false);
+    });
+  });
+});
