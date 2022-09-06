fec44d3b91c18ea9b7626afb1dadd60d3a9328d2
danielerat <davidodo2015@gmail.com>
2022-09-06 02:24:45 +0200

    Adding the headline to the Hero and all the way app to the mount app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <MainNav />
+  <div>
+    <MainNav />
+    <hero />
+  </div>
 </template>
 
 <script>
 import MainNav from "@/components/MainNav.vue";
+import Hero from "@/components/Hero.vue";
 export default {
   name: "App",
   components: {
     MainNav,
+    Hero,
   },
 };
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <section class="flex flex-col h-screen pt-10 pb-20 bg-cyan-900">
+      <div class="grid grid-cols-12">
+        <div class="col-span-1"></div>
+        <div class="col-span-5 border border-b-amber-500"><headline /></div>
+        <div class="col-span-5 border border-b-amber-500">column 3</div>
+        <div class="col-span-1"></div>
+      </div>
+    </section>
+  </main>
+</template>
+<script>
+import Headline from "@/components/Headline";
+export default {
+  name: "Hero",
+  components: {
+    Headline,
+  },
+};
+</script>
