9358ee55c671fc3359a117c5d77e85c21c23cb4b
danielerat <davidodo2015@gmail.com>
2022-09-10 07:57:06 +0200

    Adding Vue Route and Configure it

---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1 class="Text-6xl">This is an about page</h1>
+</template>
+<script>
+export default {
+  name: "AboutVue",
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <img class="mx-auto" alt="Vue logo" src="../assets/vueLogo.png" />
+    <h1 class="text-6xl font-bold">THis the home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
