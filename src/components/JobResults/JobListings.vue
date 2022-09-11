610d476e41ad5a2667ab36a1a0d8dce0400dedfb
danielerat <davidodo2015@gmail.com>
2022-09-11 12:10:56 +0200

    Adding pagination to our jobListings

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,8 +1,29 @@
 <template>
   <main class="flex-auto p-8 bg-brand-gray-2">
     <ol>
-      <job-listing v-for="job in jobs" :key="job.id" :job="job" />
+      <job-listing v-for="job in displayedJobs" :key="job.id" :job="job" />
     </ol>
+    <div class="mt-8 mx-auto">
+      <div class="flex flex-nowrap">
+        <p class="text-sm flex-grow">Page {{ currentPage }}</p>
+        <div class="flex">
+          <router-link
+            v-if="previousPage"
+            :to="{ name: 'JobResults', query: { page: previousPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+          >
+            Previous
+          </router-link>
+          <router-link
+            v-if="nextPage"
+            :to="{ name: 'JobResults', query: { page: nextPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+          >
+            NextPage
+          </router-link>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 <script>
@@ -18,8 +39,32 @@ export default {
       jobs: [],
     };
   },
+  computed: {
+    currentPage() {
+      const pageString = this.$route.query.page || 1; //the || 1 is a fall back
+      return Number.parseInt(pageString);
+    },
+    previousPage() {
+      const previousPage = this.currentPage - 1;
+      const firstPage = 1;
+      return previousPage >= firstPage ? previousPage : undefined;
+    },
+    nextPage() {
+      const nextPage = this.currentPage + 1;
+      const maxPage = Math.ceil(this.jobs.length / 10);
+      return nextPage <= maxPage ? nextPage : undefined;
+    },
+
+    displayedJobs() {
+      const pageNumber = this.currentPage;
+      const firstJobIndex = (pageNumber - 1) * 10;
+      const lastJobIndex = pageNumber * 10;
+      return this.jobs.slice(firstJobIndex, lastJobIndex);
+    },
+  },
   async mounted() {
-    const response = await axios.get("http://localhost:3000/jobs");
+    const baseUrl = process.env.VUE_APP_API_URL;
+    const response = await axios.get(`${baseUrl}/jobs`);
     this.jobs = response.data;
   },
 };
