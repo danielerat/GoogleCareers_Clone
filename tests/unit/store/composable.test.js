8004f7854ddc13abe0ff7c8d7cfcf3dec7582180
danielerat <davidodo2015@gmail.com>
2022-09-19 19:43:20 +0200

    Adding more composable for our JOB_TYPE and ORGANIZATIONS

--- tests/unit/store/composable.test.js
+++ tests/unit/store/composable.test.js
@@ -1,16 +1,44 @@
 import { useStore } from "vuex";
 jest.mock("vuex");
 
-import { useFilteredJobs } from "@/store/composable";
+import {
+  useFilteredJobs,
+  useUniqueJobTypes,
+  useuniqueOrganizations,
+} from "@/store/composable";
 
 describe("Composables", () => {
   describe("UsefilteredJobs", () => {
-    useStore.mockReturnValue({
-      getters: {
-        FILTERED_JOBS: [{ ID: 1 }],
-      },
+    it("Retrieve filtered jobs from store", () => {
+      useStore.mockReturnValue({
+        getters: {
+          FILTERED_JOBS: [{ ID: 1 }],
+        },
+      });
+      const results = useFilteredJobs();
+      expect(results.value).toEqual([{ id: 1 }]);
     });
-    const results = useFilteredJobs();
-    expect(results.value).toEqual([{ id: 1 }]);
+  });
+  describe("useUniqueJobType", () => {
+    it("Retrieve unique job type from store", () => {
+      useStore.mockReturnValue({
+        getters: {
+          UNIQUE_JOB_TYPES: new Set(["Full-time"]),
+        },
+      });
+      const results = useUniqueJobTypes();
+      expect(results.value).toEqual(new Set(["Full-time"]));
+    });
+  });
+  describe("UseUniqueOrganizations", () => {
+    it("Retrieve unique job organizations from store", () => {
+      useStore.mockReturnValue({
+        getters: {
+          UNIQUE_JOB_TYPES: new Set(["Apple"]),
+        },
+      });
+      const results = useuniqueOrganizations();
+      expect(results.value).toEqual(new Set(["Apple"]));
+    });
   });
 });
