6999e6f2a4b3ff40a6fbaaae1175d46716aa2603
danielerat <davidodo2015@gmail.com>
2022-09-29 22:27:35 +0200

    Refactoring our store files to use test script

--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -1,20 +1,23 @@
+import { GlobalState } from "@/store/types";
+import { Job } from "@/api/types";
 import {
   LOGIN_USER,
   RECEIVE_JOBS,
   ADD_SELECTED_ORGANIZATIONS,
   ADD_SELECTED_JOB_TYPES,
 } from "@/store/constants";
+
 const mutations = {
-  [LOGIN_USER](state) {
+  [LOGIN_USER](state: GlobalState) {
     state.isLoggedIn = true;
   },
-  [RECEIVE_JOBS](state, jobs) {
+  [RECEIVE_JOBS](state: GlobalState, jobs: Job[]) {
     state.jobs = jobs;
   },
-  [ADD_SELECTED_ORGANIZATIONS](state, organizations) {
+  [ADD_SELECTED_ORGANIZATIONS](state: GlobalState, organizations: string[]) {
     state.selectedOrganizations = organizations;
   },
-  [ADD_SELECTED_JOB_TYPES](state, selectedJobTypes) {
+  [ADD_SELECTED_JOB_TYPES](state: GlobalState, selectedJobTypes: string[]) {
     state.selectedJobTypes = selectedJobTypes;
   },
 };
---
+++ src/store/types.ts
@@ -0,0 +1,7 @@
+import { Job } from "@/api/types";
+export interface GlobalState {
+  isLoggedIn: boolean;
+  jobs: Job[];
+  selectedOrganizations: string[];
+  selectedJobTypes: string[];
+}
